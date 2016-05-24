298aeaa030fb466e76d2230a411f39b7b8042819
eva1b430 <448082502@qq.com>
2016-05-24 20:55:20 +0800

    编写武器系统的，

--- mini_hammer/Common/Armory/Weapons/WeaponSystem.h
+++ mini_hammer/Common/Armory/Weapons/WeaponSystem.h
@@ -1,21 +1,29 @@
 #ifndef WEAPONSYSTEM_H
 #define WEAPONSYSTEM_H
 
+#include "BaseEntity.h"
+#include <map>
+
+
+using namespace std;
 class Vehicle;
+class Weapon;
 class WeaponSystem
 {
 public:
-	WeaponSystem(Vehicle* pVehicle)
-		: m_pVehicle(pVehicle)
-	{
-
-	}
-
+	WeaponSystem(Vehicle* pVehicle);
 	~WeaponSystem(void);
+
+	// 添加武器到武器表
+	void AddWeapon(BaseEntity::BaseEntity_Type entity_type);
 
 private:
 	// 拥有武器系统的智能体
-	Vehicle*			m_pVehicle;
+	Vehicle*									m_pVehicle;
+	// 武器表
+	map<BaseEntity::BaseEntity_Type, Weapon*>	m_WeaponMap;
+	// 当前武器
+	Weapon*										m_pCurrentWeapon;
 };
 #endif // WEAPONSYSTEM_H
 
